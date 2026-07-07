d9086d5b1b5d5880b7759c3e1d8f1c8b614ac76c
sridharkuruva888 <sridharkuruva888@gmail.com>
2026-07-07 13:43:46 +0530

    Add files via upload

--- Map API/index.js
+++ Map API/index.js
@@ -2,55 +2,98 @@ const apiKey = "SkJ5bYYcUlmdxlmQtoYE";
 
 maptilersdk.config.apiKey = apiKey;
 
+
 const map = new maptilersdk.Map({
     container: "map",
     style: maptilersdk.MapStyle.STREETS,
-    center: [78.4867, 17.3850], // Hyderabad
-    zoom: 5
+    center: [78.4867, 17.3850], 
+    zoom: 18
 });
 
 let marker;
 
-async function searchPlace(){
+
+if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition(
+
+        (position) => {
+
+            const lng = position.coords.longitude;
+            const lat = position.coords.latitude;
+
+            
+            map.flyTo({
+                center: [lng, lat],
+                zoom: 14
+            });
+
+            
+            marker = new maptilersdk.Marker({
+                color: "red"
+            })
+            .setLngLat([lng, lat])
+            .addTo(map);
+
+        },
+
+        (error) => {
+            console.log("Location access denied.", error);
+            alert("Unable to access your location.");
+        }
+
+    );
+
+} else {
+    alert("Geolocation is not supported by this browser.");
+}
+
+
+async function searchPlace() {
 
     const place = document.getElementById("place").value.trim();
 
-    if(place === ""){
+    if (place === "") {
         alert("Please enter a place");
         return;
     }
 
     const url = `https://api.maptiler.com/geocoding/${encodeURIComponent(place)}.json?key=${apiKey}`;
 
-    try{
+    try {
 
         const response = await fetch(url);
         const data = await response.json();
 
-        if(data.features.length === 0){
+        if (data.features.length === 0) {
             alert("Place not found");
             return;
         }
 
         const coordinates = data.features[0].center;
 
+        
         map.flyTo({
             center: coordinates,
-            zoom: 12
+            zoom: 14
         });
 
-        if(marker){
+
+        if (marker) {
             marker.remove();
         }
 
-        marker = new maptilersdk.Marker()
-            .setLngLat(coordinates)
-            .addTo(map);
+        marker = new maptilersdk.Marker({
+            color: "blue"
+        })
+        .setLngLat(coordinates)
+        .addTo(map);
 
-    }
-    catch(error){
-        alert("Something went wrong!");
+    } catch (error) {
+
         console.log(error);
+        alert("Something went wrong!");
+
     }
 
 }
